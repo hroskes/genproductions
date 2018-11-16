adbc3b82f4f70ecd1380f9ce4a0e296010e31801
Heshy Roskes <jroskes1@jhu.edu>
2018-11-16 15:03:50 +0100

    add scale variations for 2018

--- python/ThirteenTeV/Hadronizer/Hadronizer_TuneCP2_13TeV_pTmaxMatch_1_pTmaxFudge_quarter_LHE_pythia8_cff.py
+++ python/ThirteenTeV/Hadronizer/Hadronizer_TuneCP2_13TeV_pTmaxMatch_1_pTmaxFudge_quarter_LHE_pythia8_cff.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 from Configuration.Generator.Pythia8CommonSettings_cfi import *
 from Configuration.Generator.MCTunes2017.PythiaCP2Settings_cfi import *
+from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
 
 generator = cms.EDFilter("Pythia8HadronizerFilter",
     maxEventsToPrint = cms.untracked.int32(1),
@@ -12,6 +13,7 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
     PythiaParameters = cms.PSet(
         pythia8CommonSettingsBlock,
         pythia8CP2SettingsBlock,
+        pythia8PSweightsSettingsBlock,
         processParameters = cms.vstring(
             'SpaceShower:pTmaxMatch = 1',
             'TimeShower:pTmaxMatch = 1',
@@ -20,6 +22,7 @@ generator = cms.EDFilter("Pythia8HadronizerFilter",
         ),
         parameterSets = cms.vstring('pythia8CommonSettings',
                                     'pythia8CP2Settings',
+                                    'pythia8PSweightsSettings',
                                     'processParameters',
                                     )
     )
